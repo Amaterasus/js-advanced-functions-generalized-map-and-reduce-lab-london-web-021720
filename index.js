c9ef99a2f619f8a3364e2496c23ecdc2d283e075
Amaterasus <skyetay03@googlemail.com>
2020-04-02 07:32:12 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map(src, func)
+{
+    const output = []
+
+    src.forEach(element => { output.push(func(element))});
+
+    return output
+}
+
+function reduce(src, func, start)
+{
+    let accum = start === undefined && typeof src.slice(-1)[0] === "number" ? accum = 0 
+                : start !== undefined ? accum = start : accum = true
+
+    src.forEach(element => { accum = func(element, accum) });
+    return accum
+}
